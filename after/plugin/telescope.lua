dc606d13be508b8ce47041b585815171b4e4b8c4
oldcastor <oldcastor@gmail.com>
2024-04-29 14:38:21 +0300

    telescope buffers and remove buffer

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,19 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
+
+local actions = require('telescope.actions')
+
+require('telescope').setup {
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<leader>bd"] = actions.delete_buffer + actions.move_to_top
+        }
+      }
+    }
+  }
+}
