4374f9cea8d864afb4b16c1e1ddb0e16e198ab1f
oldcastor <oldcastor@gmail.com>
2024-06-12 08:32:44 +0300

    migrated to lazy;

--- lua/oldcastor/init.lua
+++ lua/oldcastor/init.lua
@@ -1,2 +1,33 @@
 require("oldcastor.remap")
 require("oldcastor.set")
+require("oldcastor.lazy")
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufNewFile' }, {
+  pattern = '*.blade.php',
+  command = 'set filetype=blade',
+})
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- lsp mappings
+
+local lspGroup = augroup('lspGroup', {})
+
+autocmd('LspAttach', {
+  group = lspGroup,
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    vim.keymap.set("n", "<leader>vo", function() vim.lsp.omnifunc() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  end
+})
---
+++ lua/oldcastor/lazy_plugins/harpoon.lua
@@ -0,0 +1,15 @@
+return {
+  'theprimeagen/harpoon',
+  config = function()
+    local mark = require('harpoon.mark')
+    local ui = require('harpoon.ui')
+
+    vim.keymap.set('n', "<leader>i", mark.add_file)
+    vim.keymap.set('n', "<M-p>", ui.toggle_quick_menu)
+
+    vim.keymap.set("n", "<leader>a", function() ui.nav_file(1) end)
+    vim.keymap.set("n", "<leader>s", function() ui.nav_file(2) end)
+    vim.keymap.set("n", "<leader>d", function() ui.nav_file(3) end)
+    vim.keymap.set("n", "<leader>f", function() ui.nav_file(4) end)
+  end
+}
